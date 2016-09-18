5fd31d975874818e25a26142c417163b1efe2997
Meillet Robin <robin.meillet@epitech.eu>
2016-09-18 04:47:28 +0200

    TransactionForm submit convert to Ramda

--- client/containers/TransactionForm/TransactionForm.jsx
+++ client/containers/TransactionForm/TransactionForm.jsx
@@ -1,15 +1,19 @@
 import React, { PropTypes } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { ifElse, anyPass, propSatisfies, F } from 'ramda';
 
 const { func } = PropTypes;
 
-const submitForm = ({ value, description }, action) => {
-  if (value === 0 || isNaN(value) || description.length === 0) {
-    return;
-  }
-
-  action({ value, description });
-};
+const submitForm = ({ value, description }, action) =>
+  ifElse(
+    anyPass([
+      propSatisfies(x => x.length === 0, 'description'),
+      propSatisfies(x => x === 0, 'value'),
+      propSatisfies(isNaN, 'value'),
+    ]),
+    F,
+    () => action({ value, description }),
+  )({ value, description });
 
 const TransactionForm = reduxForm({ form: 'TRANSACTION_FORM' })(({ action, handleSubmit }) =>
   <div>
