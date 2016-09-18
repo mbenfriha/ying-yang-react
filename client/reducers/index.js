892a6ec0418b5220f97ccab85a1255903615d384
Meillet Robin <robin.meillet@epitech.eu>
2016-09-18 03:39:51 +0200

    Add Redux Form reducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form'
 
 import transactions from './transactions';
 
@@ -7,4 +8,5 @@ import transactions from './transactions';
  */
 export default combineReducers({
   transactions,
+  form: formReducer,
 });
