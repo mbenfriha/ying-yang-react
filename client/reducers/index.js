90b1e6ef4ef5b3d48271ea7b696a7e773c5af075
Meillet Robin <robin.meillet@epitech.eu>
2016-09-19 04:55:50 +0200

    Remove comment in reducer index

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -3,9 +3,6 @@ import { reducer as form } from 'redux-form';
 
 import transactions from './transactions';
 
-/**
- * Routing to be implemented
- */
 export default combineReducers({
   transactions,
   form,
