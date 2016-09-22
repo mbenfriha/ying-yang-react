c0e3f7a30f3b9c8c8ab91a5c8089b0188446b1ee
Meillet Robin <robin.meillet@epitech.eu>
2016-09-22 05:42:09 +0200

    Reformat some code

--- __env__/providers/index.js
+++ __env__/providers/index.js
@@ -17,7 +17,9 @@ const ext = pipe(
   map(prop('ext')),
   flatten,
   uniq,
-  reduce(({ resolve: { extensions } }, key) => ({ resolve: { extensions: [...extensions, key] } }), { resolve: { extensions: [] } })
+  reduce(({ resolve: { extensions } }, key) =>
+    ({ resolve: { extensions: [...extensions, key] } }), { resolve: { extensions: [] } }
+  )
 );
 
 const wrap = pipe(
@@ -29,19 +31,25 @@ const wrap = pipe(
 const pre = pipe(
   map(prop('pre')),
   notEmpty,
-  reduce(({ preLoaders }, obj) => ({ preLoaders: [...preLoaders, obj] }), { preLoaders: [] })
+  reduce(({ preLoaders }, obj) =>
+    ({ preLoaders: [...preLoaders, obj] }), { preLoaders: [] }
+  )
 );
 
 const loader = pipe(
   map(prop('loader')),
   notEmpty,
-  reduce(({ loaders }, obj) => ({ loaders: [...loaders, obj] }), { loaders: [] })
+  reduce(({ loaders }, obj) =>
+    ({ loaders: [...loaders, obj] }), { loaders: [] }
+  )
 );
 
 const post = pipe(
   map(prop('post')),
   notEmpty,
-  reduce(({ postLoaders }, obj) => ({ postLoaders: [...postLoaders, obj] }), { postLoaders: [] })
+  reduce(({ postLoaders }, obj) =>
+    ({ postLoaders: [...postLoaders, obj] }), { postLoaders: [] }
+  )
 );
 
 const applyLoaders = loaders => provider =>
@@ -56,17 +64,11 @@ const applyLoaders = loaders => provider =>
 
 const applyMixins = mixins => provider => mergeAll([provider, ...mixins]);
 
-const applyPlugins = plugins => provider => mergeAll([provider, { plugins: flatten(plugins.map(plugin => plugin(provider))) }]);
+const applyPlugins = plugins => provider =>
+  mergeAll([provider, { plugins: flatten(plugins.map(plugin => plugin(provider))) }]);
 
-const provide = provider => mixins => loaders => plugins => {
-  const a = applyMixins(mixins)(provider);
-  const b = applyLoaders(loaders)(a);
-  const c = applyPlugins(plugins)(b);
-
-  console.log(c);
-
-  return c;
-};
+const provide = provider => mixins => loaders => plugins =>
+  applyPlugins(plugins)(applyLoaders(loaders)(applyMixins(mixins)(provider)));
 
 /**
  * Public API - WebPack configuration builder
