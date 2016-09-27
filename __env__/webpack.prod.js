721a4fa2b860d59d00851646fd5fada17403b866
kMeillet <robin.meillet@epitech.eu>
2016-09-27 23:03:08 +0200

    No devTool in production

--- __env__/webpack.prod.js
+++ __env__/webpack.prod.js
@@ -35,7 +35,7 @@ module.exports = builder(
   ],
   [
     DefinePlugin('production'),
-    DevToolPlugin(true),
+    DevToolPlugin(false),
     ProgressBarPlugin(),
     ChunkPlugin(),
     MinifyPlugin(),
