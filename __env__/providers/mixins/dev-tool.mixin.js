a97ad35a39ea5fc51dead5a49a0099c321eb5855
kMeillet <robin.meillet@epitech.eu>
2016-09-27 23:00:53 +0200

    DevTool Mixin remove & move to plugin

--- __env__/providers/mixins/dev-tool.mixin.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const wrapper = hasDevTool => ({
-  devTool: hasDevTool ? 'cheap-eval-source-map' : false,
-});
-
-module.exports = wrapper;
--- __env__/providers/mixins/index.js
+++ __env__/providers/mixins/index.js
@@ -1,11 +1,9 @@
 'use strict';
 
-const DevToolMixin = require('./dev-tool.mixin.js');
 const InputMixin = require('./input.mixin.js');
 const OutputMixin = require('./output.mixin.js');
 
 module.exports = {
-  DevToolMixin,
   InputMixin,
   OutputMixin,
 };
