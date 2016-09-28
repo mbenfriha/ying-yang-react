00a838ee163318c0a675869e89fa08d2302e8d63
kMeillet <robin.meillet@epitech.eu>
2016-09-28 20:05:55 +0200

    Add metadata

--- __env__/providers/plugins/define.plugin.js
+++ __env__/providers/plugins/define.plugin.js
@@ -1,12 +1,15 @@
 'use strict';
 
-const { DefinePlugin } = require('webpack');
+const { DefinePlugin } = require('webpack');
 
 const wrapper = (env, extra = {}) => provider =>
   new DefinePlugin(
     Object.assign(
       { 'process.env.NODE_ENV': JSON.stringify(env) },
-      extra
+      { NODE_ENV: JSON.stringify(env) },
+      { ENV: JSON.stringify(env) },
+      { 'process.env.METADATA': JSON.stringify(extra) },
+      { 'window.METADATA': JSON.stringify(extra) }
     )
   );
 
